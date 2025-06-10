61d6c867dd6794330952bef81ddeefab8c8ef590
Boris Ning <tpz7@cdc.gov>
2025-06-10 09:00:32 -0400

    cleanup for repositories

--- src/repository.py
+++ src/repository.py
@@ -1,5 +1,4 @@
 from github import Auth
-from github import Github
 from github import GithubIntegration
 
 class Repository:
@@ -13,22 +12,6 @@ class Repository:
     g = self.authenticate(credentials)
     repos = []
     for repo in g.get_organization(credentials.get('github_org')).get_repos(type='all'):
-      # print(repo.name)
-      # print(repo.html_url)
-      # print(repo.description)
-      # print(repo.created_at)
-      # print(repo.updated_at)
-      # print(repo.pushed_at)
-      # print(repo.language)
-      # print(repo.size)
-      # print(repo.stargazers_count)
-      # print(repo.forks_count)
-      # print(repo.open_issues_count)
-      # print(repo.watchers_count)
-      # print(repo.default_branch)
-      # print(repo.has_issues)
-      # print(repo.has_wiki)
-      # print(repo.has_downloads)
       repos.append(repo)
     return repos
 
