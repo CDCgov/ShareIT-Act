5efd50e4724908960bec88e1530aa32c097b1197
Boris Ning <41026275+boris-ning-usds@users.noreply.github.com>
2025-07-15 19:46:13 -0400

    fix: hiding a problem where the JWT token expires within an hour by... running faster (#60)

--- src/repository.py
+++ src/repository.py
@@ -3,32 +3,37 @@ from github import Github
 from github import GithubIntegration
 
 class Repository:
-  def authenticate(self, credentials):
-    app_id = credentials.get('github_app_id', '')
-    installation_id = credentials.get('github_app_installation_id', '')
-    private_key = credentials.get('github_app_private_key', '')
+
+  def __init__(self, credentials=None):
+    self.credentials = credentials
+
+  def authenticate(self):
+    app_id = self.credentials.get('github_app_id', '')
+    installation_id = self.credentials.get('github_app_installation_id', '')
+    private_key = self.credentials.get('github_app_private_key', '')
 
     ## Use the Github personal access token for authentication
     ## Otherwise, use GitHub App authentication
     ## This is just a personal preference, either is fine.
-    if 'github_token' in credentials and credentials['github_token']:
-      return Github(credentials['github_token'])
+    if 'github_token' in self.credentials and self.credentials['github_token']:
+      return Github(self.credentials['github_token'])
     auth = Auth.AppAuth(app_id, private_key)
     gi = GithubIntegration(auth=auth)
     access_token = gi.get_access_token(installation_id).token
     return Github(access_token)
 
-  def get_repos(self, credentials):
-    g = self.authenticate(credentials)
-    org_name = credentials.get('github_org')
+  def get_repos(self):
+    g = self.authenticate()
+    org_name = self.credentials.get('github_org')
     org = g.get_organization(org_name)
     repos = list(
       org.get_repos(type='all')
     )
+    print(f"Found {len(repos)} repositories in Github.com organization {org_name}")
     return repos
 
-  def get_repo_by_id(self, credentials, repo_id):
-    g = self.authenticate(credentials)
+  def get_repo_by_id(self, repo_id):
+    g = self.authenticate(self.credentials)
     try:
       repo = g.get_repo(int(repo_id))
       return repo
